ae14687c9896011b1393e731cc770143b3153b62
khushigupta515 <56690338+khushigupta515@users.noreply.github.com>
2020-08-19 16:45:03 +0530

    Update adduser.php

--- adduser.php
+++ adduser.php
@@ -1,3 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <title>CREDIT MANAGEMENT SYSTEM</title>
+  
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <!-- navbar google font -->
+  <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
+   <!-- herotext class -->
+  <link href="https://fonts.googleapis.com/css2?family=Patua+One&display=swap" rel="stylesheet">
+  <!-- body  -->
+  <link href="https://fonts.googleapis.com/css2?family=Martel:wght@200&family=Patua+One&display=swap" rel="stylesheet">
+  
+
+    <!-- External Style Sheet -->
+    <link rel="stylesheet" type="text/css" href="stylespark.css">
+    <style type="text/css">
+      body
+      {margin: 0;
+       padding: 0;
+       }
+    </style>
+    
+   
+
+  </head>
+
+
 <?php
 
 $dbServername = "localhost";
@@ -17,3 +56,13 @@ if ($conn->query($sql) === TRUE) {
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
+?>
+<body>
+ <div class="container-fluid" id="viewuser" style="padding-left: 0%;padding-right: 0%;" >
+  <div class="jumbotron text-center" style="margin-bottom: 0%;" >
+    <h2 class="text-center">Manage your Credits</h2>
+    <p>Click to view the list of users and their credits</p>
+    <button style="background: #56A49C;color: black;border-radius: 20%;padding: 1%;"><a href='printtable.php' style="color:black;text-decoration: none;">View Users</a></button>
+    <button style="background: #56A49C;color: black;border-radius: 20%;padding: 1%;margin-left: 1%; " ><a href='creditmanagement1.html' style="color:black;text-decoration: none;"> Home</a></button>
+  </div>
+  </body>
